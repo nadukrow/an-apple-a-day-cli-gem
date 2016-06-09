36ba537b082e7b83eafd49aa3237d7ae913a8f1f
nadukrow <nadukrow@gmail.com>
2016-06-09 18:40:53 -0400

    adding placeholders to what I want my CLI to put to the user

--- lib/an_apple_a_day/cli.rb
+++ lib/an_apple_a_day/cli.rb
@@ -3,6 +3,16 @@ class AnAppleADay::CLI
 
   def call
     puts "Primary Care Providers in DC"
+    list_providers
+    zip
+  end
+
+  def list_providers
+    puts "Name - Address - City - State - Zip - Ward - Phone Num"
+  end
+
+  def zip
+    puts "Enter your current zip code"
   end
 
 end
