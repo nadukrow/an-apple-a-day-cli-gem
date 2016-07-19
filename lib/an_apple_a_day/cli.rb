67ba8691e74ce8039cb1712e2b0ecd82c5823296
nadukrow <nadukrow@gmail.com>
2016-07-19 12:14:06 -0400

    placing the final touches on the logic of the code flow

--- lib/an_apple_a_day/cli.rb
+++ lib/an_apple_a_day/cli.rb
@@ -2,37 +2,40 @@
 class AnAppleADay::CLI
 
   def call
+    greeting
     list_hospitals
-    ward
-    goodbye
+    list_hospitals_by_one
+  end
+
+  def greeting
+    puts "Welcome to the AnAppleADay gem! Here you can type in 'list' for a full list of hospitals."
   end
 
   def list_hospitals
+    input = gets.strip.downcase
     puts "Hospital Facilities in DC"
     puts "Name - Address - Ward - Phone Num"
+    if input == "list"
     @providers = AnAppleADay::Location.list
     @providers.each.with_index(1) do |provider, i|
       puts "#{i} - #{provider.name} - #{provider.address} - #{provider.ward} - #{provider.num}"
     end
   end
 
-  def ward
-    input = nil
-    puts "You can either enter your DC ward number for the closest hospital, type 'hospital for the full list of hospitals within DC' or type 'exit' to leave."
+  def list_hospitals_by_one
+    puts "You can take a look at a specific hospital by typing in its corresponding number."
+    input = input.to_i
     while input!= "exit"
       input = gets.strip
       if input.to_i > 0
         provider_selected = @providers[input.to_i-1]
-        puts "#{provider.name} - #{provider.address} - #{provider.ward} - #{provider.num}"
+        puts "#{provider_selected.name} - #{provider_selected.address} - #{provider_selected.ward} - #{provider_selected.num}"
       elsif input == "hospitals"
         list_hospitals
       else
-        puts "These aren't the droids you're looking for, type a DC ward number, 'hospitals' or 'exit'."
+        puts "An apple a day keeps the doctor away!"
       end
     end
   end
-
-  def goodbye
-      puts "An apple a day keeps the doctor away!"
-    end
-  end
+end
+end
