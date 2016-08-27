07047d6ff5062aa569f5db0624e5e11b28b19aa6
nadukrow <nadukrow@gmail.com>
2016-08-27 18:53:45 -0400

    adding comments to version

--- lib/an_apple_a_day/version.rb
+++ lib/an_apple_a_day/version.rb
@@ -1,3 +1,6 @@
 module AnAppleADay
   VERSION = "0.1.1"
 end
+
+#version 0.1.0 was the initial version before refactoring. working towards splitting the relevant classes
+#version 0.1.1 is the inclusion of a new class the holds the locations.
